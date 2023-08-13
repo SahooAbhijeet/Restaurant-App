8dc8536d40463299291e3d9cab0210ff453c8173
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-08-13 11:16:26 +0530

    Added header to the app

--- App.js
+++ App.js
@@ -1,36 +1,51 @@
 import  React from "react";
 import ReactDOM from "react-dom/client";
+/**
+ * Header 
+ *   - Logo
+ *   - Nav-items
+ *   - Cart
+ * Body
+ *    - Search-bar
+ *    - Restaurant-List
+ *        - Restaurant-Card
+ *             - Image
+ *             - Name
+ *             - Ratings
+ *             - Cusines
+ * Footer
+ *    - Links
+ *    - Copyright
+ */
+const Title = () => (
+    <a href="/">
+    <img
+    className="Logo"
+    alt="Logo" 
+    src="https://th.bing.com/th/id/OIP.sEQoAUtBqtmJdj2ycEh3XAHaHa?w=169&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7" 
+    />
+    </a>
+)
 
+const HeaderComponent = () => {
+    return (
+        <div className="Header">
+            <Title />
+            <div className="Nav-Items">
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Cart</li>
+                
+                </ul>
+            </div>
+        </div>
+    );
+};
 
-const heading = React.createElement(
-    "h1", 
-    {
-      id: "title",
-      key: "h1",
-    }, 
-    "Heading 1"
-);
-
-const heading2 = React.createElement(
-    "h2", 
-    {
-      id: "title",
-      key: "h2",
-    },
-    "Heading 2"
-);
-
-const container = React.createElement(
-    "div",
-    {
-        id: "container",
-    },
-    [heading, heading2]
-);
-
-console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 // passing the react element inside the root 
 
-root.render(container);
+root.render(<HeaderComponent />);
