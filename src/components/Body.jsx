7a1b5376442cf7a29643c63ed1b46b9b71a817b4
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-08-19 22:33:26 +0530

    Added the filtering logic to the resturant-app

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,48 @@
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard"
+import { useState } from "react";
+
+function filterData(searchText, restaurants) {
+  const filterData = restaurants.filter((restaurant) => 
+    restaurant.info.name.includes(searchText)
+  );
+  
+  return filterData;
+}
 
 const Body = () => {
-    return (
-        <div className="restaurant-list">
+      const [restaurants, setrestaurants] = useState(restaurantList);
+      const [searchText, setsearchText] = useState();
+
+      return (
+      <>
+      <div className="search-container">
+        <input
+        type="text"
+        className="search-input"
+        placeholder="Search"
+        value={searchText}
+        onChange={(e) => {
+          setsearchText(e.target.value);
+        }}
+        />
+        <button 
+        className="search-btn" 
+        onClick={() => {
+        const data = filterData(searchText, restaurants);
+          setrestaurants(data);
+        }} 
+        > 
+        Search
+      </button>
+    </div>
+
+      <div className="restaurant-list">
         {restaurantList.map((restaurant) => {
           return <RestaurantCard {...restaurant.info} />;
         })}
         </div>
+        </>
     );
 };
 export default Body;
