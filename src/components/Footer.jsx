5d85b046170ec197454df6e10d063dbcb109e9c8
ABHIJEET <abhijeetsahoomsd@gmail.com>
2023-09-20 12:25:10 +0530

    added footer page to our app and installed the react-icons package

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,39 @@
 import { useContext } from  "react";
 import UserContext from "../utils/UserContext";
+import { BsFacebook, BsInstagram, BsLinkedin, BsTwitter } from "react-icons/bs";
 
 const Footer = () => {
     const {user} = useContext(UserContext);
+    const currentdate = new Date();
+    const year = currentdate.getFullYear();
 
     return (
+        <>
+        <footer className="relative left-0 bottom-0 h-10 py-8 flex flex-col sm:flex-row items-center justify-between text-white bg-gray-800 sm:px-20">
+            <section className="text-2xl ">
+                Copyright {year} | All Rights Reserved
+            </section>
+            <section className="flex items-center justify-center gap-5 text-3xl text-white">
+                <a className="hover:text-yellow-400 transition-all ease-in-out duration-300">
+                    <BsFacebook />
+                </a>
+                <a className="hover:text-yellow-400 transition-all ease-in-out duration-300">
+                    <BsInstagram />
+                    </a>
+                <a className="hover:text-yellow-400 transition-all ease-in-out duration-300">
+                    <BsLinkedin />
+                    </a>
+                <a className="hover:text-yellow-400 transition-all ease-in-out duration-300">
+                    <BsTwitter />
+                </a>
+            </section>
+        </footer>
+
         <h4 className="p-10 m-10"> 
         This site is developed by {user.name} - {user.email}
         </h4>
+        </>
+       
     ); 
 };
 
